63d47c02770e2518ada4a6d9b601f349d8fec67d
melisabeatriz <melisabeatriz@gmail.com>
2025-07-05 18:43:55 -0300

    minor style changes, basic logic for mapping section

--- src/pages/Letters.js
+++ src/pages/Letters.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Letters.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
@@ -7,33 +8,73 @@ import picMmChillar from "../img/pic-01_mmChillar37.jpg";
 const lettersMapping = [
   {
     letterSrc: aMmChillar,
-    letterReference: "Matadero Municipal Chillar • 1937",
+    letterReference: "Matadero Municipal",
+    place: "Chillar",
+    year: 1937,
     originalPhoto: picMmChillar,
-    category: "individualLetterA",
+    isInvidualLetter: true,
+    isDrawnLetter: false,
+    category: "A",
+    description: "Realizada en hormigón",
   },
 ];
 
-const Letter = ({ letterSrc, letterReference, originalPhoto }) => (
-  <div className="mapping-letter">
-    <img src={letterSrc} width="400" alt={letterReference} />
-    <span>{letterReference}</span>
-    <img src={originalPhoto} />
+const LetterModal = ({ closeModal, modalInfo }) => (
+  <div className="letter-modal">
+    <p>{modalInfo.place}</p>
+    <span>{modalInfo.letterReference}</span>
+    <button onClick={() => closeModal()}>x</button>
+    <img src={modalInfo.originalPhoto} />
   </div>
 );
 
-const Letters = () => (
-  <div className="letters-container">
-    <NavBar show />
-    {lettersMapping.map((letter, i) => (
-      <Letter
-        key={i}
-        letterSrc={letter.letterSrc}
-        letterReference={letter.letterReference}
-        originalPhoto={letter.originalPhoto}
-      />
-    ))}
-    <Footer />
-  </div>
-);
+const Letter = ({
+  letterSrc,
+  info,
+  setModalInfo,
+  letterReference,
+  showModal,
+}) => {
+  return (
+    <div
+      className="mapping-letter"
+      onClick={() => {
+        setModalInfo(info);
+        showModal();
+      }}
+    >
+      <img src={letterSrc} width="400" alt={letterReference} />
+    </div>
+  );
+};
+
+const Letters = () => {
+  const [modalInfo, setModalInfo] = useState({});
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => setIsModalVisible(true);
+  const closeModal = () => setIsModalVisible(false);
+
+  return (
+    <div className="letters-container">
+      <NavBar show />
+      {lettersMapping.map((letter, i) => (
+        <Letter
+          key={i}
+          letterSrc={letter.letterSrc}
+          letterReference={letter.letterReference}
+          originalPhoto={letter.originalPhoto}
+          info={letter}
+          setModalInfo={setModalInfo}
+          showModal={showModal}
+        />
+      ))}
+      {isModalVisible && (
+        <LetterModal closeModal={closeModal} modalInfo={modalInfo} />
+      )}
+      <Footer />
+    </div>
+  );
+};
 
 export default Letters;
